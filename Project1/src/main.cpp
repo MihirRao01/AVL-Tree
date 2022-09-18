3b1644dd546df884f51a178347dee88c973698b9
MihirRao01 <mihirhrao@gmail.com>
2022-09-18 16:04:33 -0400

    added name to the notes

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -3,6 +3,7 @@
 /* Note: 
 	1. You will have to comment main() when unit testing your code because catch uses its own main().
 	2. You will submit this main.cpp file and any header files you have on Gradescope. 
+	3. Mihir Rao
 */
 
 int main(){
