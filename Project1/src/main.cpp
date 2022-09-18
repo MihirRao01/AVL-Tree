770cfd300f5e74a7f6175a0a4555d3c3f9721248
MihirRao01 <mihirhrao@gmail.com>
2022-09-18 16:08:57 -0400

    Update main.cpp

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -4,6 +4,7 @@
 	1. You will have to comment main() when unit testing your code because catch uses its own main().
 	2. You will submit this main.cpp file and any header files you have on Gradescope. 
 	3. Mihir Rao
+	4. Due Oct 3
 */
 
 int main(){
